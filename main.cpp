8fdf267b59821d377b9450bcbec979fcf8563526
only1firas <62120706+only1firas@users.noreply.github.com>
2020-04-05 21:53:02 -0700

    inital

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include "Store.h"
+#include "Product.h"
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    return 0;
+}
